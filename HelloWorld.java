3b16a8251b1deaf7f630717aa3421d8201d5e1ad
grvsa <47443539+grvsa@users.noreply.github.com>
2019-03-04 21:14:39 +0300

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.hello_world;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Git. Быстрый старт. Java. ");
-        System.out.println("Hello World");
-    }
-}
