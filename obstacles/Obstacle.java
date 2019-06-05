8ecb430db5e98157f66902335a81cad54675c59f
grvsa <47443539+grvsa@users.noreply.github.com>
2019-06-05 14:59:07 +0300

    Java2 Lesson 1 HomeWork

---
+++ obstacles/Obstacle.java
@@ -0,0 +1,8 @@
+package Lesson_1.Marafon.obstacles;
+
+import Lesson_1.Marafon.competitors.Competitor;
+
+public abstract class Obstacle {
+    public abstract void doIt(Competitor competitor);
+}
+
---
+++ obstacles/Wall.java
@@ -0,0 +1,16 @@
+package Lesson_1.Marafon.obstacles;
+
+import Lesson_1.Marafon.competitors.Competitor;
+
+public class Wall extends Obstacle {
+    int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void doIt(Competitor competitor) {
+        competitor.jump(height);
+    }
+}
