c34d9c958426c056b98928dcb1d1995891ce5ba2
grvsa <47443539+grvsa@users.noreply.github.com>
2019-07-04 09:00:04 +0300

    Java3Lesson1 Home Work
    Generics
    - Array elements exchange
    - Array to List conversion
    - Box implementation

---
+++ fruits/Orange.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java3.lesson1.fruits;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5f);
+    }
+
+    public Orange(float weight) {
+        super(weight);
+    }
+}
